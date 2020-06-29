166f816effba5c215baffbb78e578e4060834e55
Maika Kailani <queenmab051@gmail.com>
2020-06-29 21:28:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(array)
     if array == languages
       array.collect do |x| x.upcase
-      end
-
+    end
     else
       array.collect do |x| x.split(" ").first
-      end
-
     end
+  end
 end
